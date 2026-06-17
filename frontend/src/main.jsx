e1cc267a72931f76619239e3fbc46d737b3f5efe
Sakib Mohammed Sobaha <1905021@ugrad.cse.buet.ac.bd>
2026-06-17 22:52:06 +0600

    font changed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/cascadia-code/400.css'
+import '@fontsource/cascadia-code/600.css'
+import '@fontsource/cascadia-code/700.css'
 import './index.css'
 import App from './App.jsx'
 
